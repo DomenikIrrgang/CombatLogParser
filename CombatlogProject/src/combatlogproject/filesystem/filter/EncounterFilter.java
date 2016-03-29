ea0173cd5f14c31c8869a957a51096642b623ecd
Domenik Irrgang <domenikirrgang@web.de>
2016-03-29 21:46:00 +0200

    init combatlogproject.filesystem

---
+++ CombatlogProject/src/combatlogproject/filesystem/filter/EncounterFilter.java
@@ -0,0 +1,10 @@
+package combatlogproject.filesystem.filter;
+
+/**
+ * Searches for a specific encounter in a combatlogfile.
+ * 
+ * @author Domenik Irrgang
+ */
+public class EncounterFilter {
+    
+}
