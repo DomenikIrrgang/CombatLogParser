197576effcfbf09f4f8632ea7c57eba1f0856da8
Marc <Marc@Marc-PC>
2016-03-29 23:29:29 +0200

    Implementation of Combat Log Entry.

--- CombatlogProject/src/combatlogproject/filesystem/CombatlogEntry.java
+++ CombatlogProject/src/combatlogproject/filesystem/CombatlogEntry.java
@@ -6,5 +6,29 @@ package combatlogproject.filesystem;
  * @author Domenik Irrgang
  */
 public class CombatlogEntry {
+    private long timestamp;
+    private String[] args;
+    private CombatlogEvent CombatlogEvent;
     
-}
+    
+   public CombatlogEntry (long timestamp, String[] args)
+   {
+       this.timestamp=timestamp;
+       this.args=args;
+       
+   }
+   public String[] getArgs(){
+       return args;
+   }
+   
+   public long getTimestamp(){
+       return timestamp;
+   }
+   
+   public CombatlogEvent getCombatlogEvent(){
+       return CombatlogEvent;
+   }
+   }
+   
+   
+
