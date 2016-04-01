0d9202b72cf117dc23953b3b52a592d17360962a
Marc <Marc@Marc-PC>
2016-04-01 14:01:01 +0200

    Implementation of Unit

---
+++ CombatlogProject/src/combatlogproject/filesystem/Unit.java
@@ -0,0 +1,18 @@
+
+package combatlogproject.filesystem;
+
+/**
+ *
+ * @author Marc
+ */
+public class Unit {
+  public String name;
+  
+  public Unit(String name){
+      this.name=name;
+  }
+  
+  public String getName(){
+      return name;
+  }
+}
